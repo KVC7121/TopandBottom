a72c839d06736dd85c49098b1a9f030a537c2f8a
KVC7121 <kodichetu@gmail.com>
2023-05-24 19:02:07 +0530

    I have updated

--- app/src/main/java/com/example/topandbottom/MainActivity.kt
+++ app/src/main/java/com/example/topandbottom/MainActivity.kt
@@ -63,6 +63,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            // This is an update
             CustomScaffold()
         }
     }
